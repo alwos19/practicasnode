9e9bc9947b4663d16d6323d60f4d9af67c9d8838
raiden <jhon.jaramilloe@udea.edu.co>
2020-06-21 09:29:44 -0500

    cambio_promesas

--- promesas.js
+++ promesas.js
@@ -1,40 +1,66 @@
 let empleados = [{
     id: 1,
     nombre: 'Fernando'
-},{
+}, {
     id: 2,
-    nombre: 'Melissa'
-},{
+    nombre: 'Melisa'
+}, {
     id: 3,
     nombre: 'Juan'
-
 }];
 
 let salarios = [{
     id: 1,
     salario: 1000
 },{
-    id: 2,
+    id:2,
     salario: 2000
+
 }];
 
-let getEmpleado = (id) => {
+let getEmpleado = (id, ) =>{
 
-    return new Promise ( (resolv, reject) => { 
+    return new Promise( (resolve, reject)=>{
 
         let empleadoDB = empleados.find(empleado => empleado.id === id)
 
-        if( !empleadoDB ) {
+        if (!empleadoDB) {
             reject(`No existe un empleado con el ID ${ id }`)
         } else {
-        resolv(empleadoDB);
+            resolve(empleadoDB); 
         }
+        
     });
-
 }
 
-getEmpleado(2).then(empleado=> {
-    console.log('Empleado de BD', empleado);
-}, (err)=> {
+let getSalario = (empleado) => {
+    return new Promise((resolve, reject)=>{
+        
+        let salarioDB = salarios.find(salario => salario.id === empleado.id);
+
+        if (!salarioDB) {
+            reject(`No se encontró un salario para el usuario ${empleado.nombre}`);
+        } else {
+            resolve({
+                nombre: empleado.nombre,
+                salario: salarioDB.salario,
+                id: empleado.id
+            });
+        }
+    });
+}
+
+
+
+
+getEmpleado(3).then ( empleado => {
+
+    getSalario(empleado).then(resp => {
+        console.log(`El Salario de ${resp.nombre} es de ${ resp.salario }$`);
+    }, err => {
+        console.log(err)
+    });
+
+}, (err) => {
     console.log(err);
 });
